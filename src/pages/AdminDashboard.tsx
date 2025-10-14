09626e80386b98aae82033bff4a99be35d697260
Fabrich <hei.fabrich.2@gmail.com>
2025-10-14 13:23:09 +0300

    patch: new video language

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -67,7 +67,7 @@ const AdminDashboard = () => {
           <Link to={"/upload"} className="relative flex items-center justify-center gap-2 px-6 py-2.5
     font-medium text-sm rounded-xl transition-all duration-300
     backdrop-blur-md border cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-300 bg-white/90 hover:bg-white text-gray-800 border-gray-200 hover:border-gray-300 shadow-sm hover:shadow-md">
-            + upload
+            + new
           </Link>
         </div>
       </header>
