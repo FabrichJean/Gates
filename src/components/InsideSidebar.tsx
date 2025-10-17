61f2983826da98e744d17cf42bf20220b73f27b4
Fabrich <hei.fabrich.2@gmail.com>
2025-10-17 12:25:18 +0300

    conflit2

--- src/components/InsideSidebar.tsx
+++ src/components/InsideSidebar.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import Sidebar from "./Sidebar";
+import StickyUploadProgress from "./StickyUploadProgress";
 
 function InsideSidebar({ children }: React.PropsWithChildren) {
     const [isCollapsed, setIsCollapsed] = useState(false);
@@ -28,7 +29,7 @@ function InsideSidebar({ children }: React.PropsWithChildren) {
     return (
         <div className="w-dvw h-dvh bg-gray-200 flex overflow-hidden relative">
             <Toaster />
-
+            <StickyUploadProgress />
             {/* ✅ Sidebar Desktop */}
             {!isMobile && <Sidebar isCollapsed={isCollapsed} />}
 
