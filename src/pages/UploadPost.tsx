b4b264d98b0c0d25a944ee1ab2c80b00a8aaac78
FabrichJean <153196686+FabrichJean@users.noreply.github.com>
2025-11-07 17:59:58 +0300

    part of: MAD-292

--- src/pages/UploadPost.tsx
+++ src/pages/UploadPost.tsx
@@ -1,7 +1,7 @@
 const UploadPost = () => {
     return (
-        <div>
-            <h1 className="text-gray-800">Upload Post</h1>
+        <div className="">
+            
         </div>  
     );
 };
