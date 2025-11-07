28d7c86495f1dbc9cc0036c04469dc7aba8084e8
Najoro Fanantenana <najofanantenana@gmail.com>
2025-11-07 16:28:29 +0300

    close MAD-290

---
+++ src/components/PlatformSelectComponent.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import type { Platform } from "../hooks/usePlatform";
+import { usePlatformReactive } from "../hooks/usePlatform";
+
+interface Props {
+    onSelect?: (platform: Platform) => void;
+}
+
+const PlatformSelectComponent = ({ onSelect }: Props) => {
+    const [platforms, setPlatforms] = useState<Platform[]>([]);
+    const { data } = usePlatformReactive();
+    const [selected, setSelected] = useState<number | "">("");
+
+    useEffect(() => {
+        if (data) {
+            setPlatforms(data);
+        }
+    }, [data]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+        if (value === "" || value === "all") {
+            setSelected("");
+            return;
+        }
+        const platformId = Number(value);
+        setSelected(platformId);
+        const selectedPlatform = platforms?.find((p) => p.id === platformId);
+
+        if (selectedPlatform && onSelect) {
+            onSelect(selectedPlatform);
+        }
+    };
+
+   return (
+    <select 
+        value={selected}
+        onChange={handleChange}
+        className="w-full border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 rounded-md p-2 outline-none transition-all duration-300 text-gray-700 dark:text-gray-200 disabled:bg-gray-100 dark:disabled:bg-gray-800 disabled:text-gray-400 dark:disabled:text-gray-500">
+        <option value="" className="flex items-center justify-center">--Select a platform--</option>
+        
+        {platforms.map((platform) => (
+            <option key={platform.id} value={platform.id} className=" flex items-center justify-center g-white dark:bg-gray-700 text-gray-700 dark:text-gray-200">{platform.name}</option>
+        ))}
+    </select>
+   );
+};
+
+export default PlatformSelectComponent;
