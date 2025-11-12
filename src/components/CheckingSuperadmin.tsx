cfb46a86c38780b92b3f1f8018161a3d28d31a33
FabrichJean <153196686+FabrichJean@users.noreply.github.com>
2025-11-12 00:16:46 -0800

    close MAD-349

--- src/components/CheckingSuperadmin.tsx
+++ src/components/CheckingSuperadmin.tsx
@@ -20,30 +20,26 @@ function CheckingSuperadmin({ video, index, reFetch, user }: Props) {
   const closeModal = () => setShowModal(false);
 
   return (
-    <div className={"dropdown " +  (index === 0 ? " dropdown-end" : "dropdown-top dropdown-end")}>
+    <div
+      className={
+        "dropdown " +
+        (index === 0 ? " dropdown-end" : "dropdown-top dropdown-end")
+      }
+    >
       <div
         tabIndex={video.id}
         role="button"
         className="flex items-center gap-2 text-xs text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700 w-max p-1 px-2 rounded-md cursor-default m-auto transition-colors duration-300 border border-gray-100 dark:border-gray-600 bg-white dark:bg-gray-800"
       >
-        {video.checking === 'checked' ? 
-          <FaCheckDouble className="text-green-600 dark:text-green-400" /> : 
+        {video.checking === "checked" ? (
+          <FaCheckDouble className="text-green-600 dark:text-green-400" />
+        ) : (
           <FiHexagon className="text-gray-500 dark:text-gray-400" />
-        } 
+        )}
         <span className="text-gray-700 dark:text-gray-300">
           {video.checking === "null" ? "not ready" : video.checking}
         </span>
       </div>
-
-      {/* CheckerDrop seulement si la vidéo est vérifiable */}
-      {/* {(video.checking === "null" && user.role === "superadmin") ? null : ( */}
-        <CheckerDrop
-          video={video}
-          reFetch={reFetch}
-          user={user}
-          openRefuseModal={openModal}
-        />
-      {/* )} */}
 
       {/* Modal dynamique pour refus */}
       {showModal && (
@@ -58,6 +54,16 @@ function CheckingSuperadmin({ video, index, reFetch, user }: Props) {
           }}
         />
       )}
+
+      {/* CheckerDrop seulement si la vidéo est vérifiable */}
+      {/* {(video.checking === "null" && user.role === "superadmin") ? null : ( */}
+      <CheckerDrop
+        video={video}
+        reFetch={reFetch}
+        user={user}
+        openRefuseModal={openModal}
+      />
+      {/* )} */}
     </div>
   );
 }
