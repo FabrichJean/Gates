4dc491595c1d8574b0dfda2f0691c0848fcf1234
zzz9527777 <qliu73133@gmail.com>
2025-11-13 18:12:43 +0800

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,6 +3,7 @@ export const server = "http://localhost:3000"
 // export const server = "http://192.168.1.115:3000"
 =======
 // export const server = "http://localhost:3000"
+export const server = "http://192.168.1.199:3000"
 >>>>>>> Stashed changes
 export const apiURL = server + "/api/v1";
 export const token = () => localStorage.getItem("authToken") || "";
