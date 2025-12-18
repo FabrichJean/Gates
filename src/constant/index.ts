8eec8410dde8d5ac9b9275239dbac94e63986582
zzz9527777 <qliu73133@gmail.com>
2025-12-18 16:25:21 +0800

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,5 +1,5 @@
 
-export const server = "http://192.168.1.69:3000"
+export const server = "http://192.168.1.199:3000"
 // export const server = "http://192.168.1.199:3000"
 // export const server = "http://localhost:3000"
 export const apiURL = server + "/api/v1";
