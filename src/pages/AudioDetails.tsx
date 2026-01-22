e5a57c4cf343a469132f3ccf00e19f0313260dbc
Hubert614-oss <hubertarlin1@gmail.com>
2026-01-22 15:45:16 +0300

    Refactor AudioDetails component to improve album display and link styling

--- src/pages/AudioDetails.tsx
+++ src/pages/AudioDetails.tsx
@@ -460,28 +460,26 @@ const AudioDetails: React.FC = () => {
             )}
             {/* Si aucun album, proposer aussi le bouton */}
             {audio.type_audio === "album" && audioAlbums.length === 0 ? (
-              <div className="bg-white dark:bg-gray-800 rounded-md p-3 shadow flex items-center gap-2">
-                <Music className="w-4 h-4" />
-                <span className="font-medium">Effectif des pistes</span>
-                <a
-                  href={`/audio-albums/upload?audio_id=${audio?.id}`}
-                  className="ml-auto px-3 py-1.5 bg-indigo-600 text-white rounded hover:bg-indigo-700 text-xs font-semibold"
-                >
-                  + New piste
-                </a>
-              </div>
-            ) : (
               <>
-                <div className="bg-white dark:bg-gray-800 rounded-md p-3 shadow flex items-center gap-2">
-                  <Music className="w-4 h-4" />
-                  <a
-                    href={`/audio-albums/upload?audio_id=${audio?.id}`}
-                    className="ml-auto px-3 py-1.5 bg-indigo-600 text-white rounded hover:bg-indigo-700 text-xs font-semibold"
-                  >
-                    + New piste
-                  </a>
+                <div className="bg-white dark:bg-gray-800 rounded-md p-3 shadow">
+                  <div className=" flex items-center gap-2 pb-2">
+                    <Music className="w-4 h-4" />
+                    <span className="font-medium"></span>
+                    <Link
+                      to={`/audio-albums/upload?audio_id=${audio?.id}`}
+                      className="ml-auto px-3 py-1.5 bg-indigo-600 text-white rounded hover:bg-indigo-700 text-xs font-semibold"
+                    >
+                      + Add album
+                    </Link>
+                  </div>
+                  <div>
+                    <div className="border-b-2 border-gray-300 dark:border-gray-700 mb-2"></div>
+                    <div className="p-2">jkkjkkj</div>
+                  </div>
                 </div>
               </>
+            ) : (
+              <></>
             )}
           </motion.div>
         </div>
