dda56ef207f29bd8becddccdae09a43512845572
FabrichJean <153196686+FabrichJean@users.noreply.github.com>
2026-02-23 15:40:27 +0300

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 export const server = "http://192.168.1.97:3000"
 // export const server = "http://192.168.1.48:3000"
+=======
+// export const server = "http://192.168.1.97:3000"
+export const server = "http://192.168.1.70:3000"
+>>>>>>> Stashed changes
 // export const server = "http://localhost:3000"
 export const apiURL = server + "/api/v1";
 export const token = () => localStorage.getItem("authToken") || "";
